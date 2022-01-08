c07ebfa09f487cde36373d1f635287be436ef4a5
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-01-08 22:13:45 +0530

    1. Binding to Custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+//import { type } from 'os';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'componentDataBinding';
+  serverElements =[{type:'server',name:'TestServer',content:'Just a Test!'}];
 }
